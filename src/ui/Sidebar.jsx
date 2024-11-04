a22b9843f012bb9068396a920adb69937a2b1b40
thexro <ravyyadav97@gmail.com>
2024-11-05 00:07:57 +0530

    uploaded some dummy data

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import Logo from "./Logo"
 import MainNav from "./MainNav"
+import Uploader from "../data/Uploader"
 const StyledSidebar = styled.div`
     background-color:var(--color-grey-00);
     border-right: 2px solid var(--color-grey-100);
@@ -15,7 +16,8 @@ function Sidebar() {
   return (
     <StyledSidebar>
         <Logo/>
-        <MainNav/>
+      <MainNav />
+      <Uploader/>
     </StyledSidebar> 
   )
 }
